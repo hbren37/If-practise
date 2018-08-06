ce41be9b638300c871434b31b1a21bba1f058a12
Helen Brennan <9981703@student.toiohomai.ac.nz>
2018-08-06 16:47:44 +1200

    if practise 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+               { Console.WriteLine("Please enter a number");
+
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a second number");
+
+                int num2 = int.Parse(Console.ReadLine());
+
+                if (num1 > num2) {
+                    Console.WriteLine("is larger");
+                    }
+                else if (num1 == num2) {
+
+                Console.WriteLine("Both these numbers are equal");
+                }
+
+                {
+                    Console.WriteLine("Please type any number between 1 and 10");
+                    int userNum = int.Parse(Console.ReadLine());
+                    int myNum = 7;
+                    Console.WriteLine("Can you guess my number between 1-10?");
+                }
+                if (myNum >5) 
+
+
+
+                { Console.WriteLine("What is your final mark of the year");
+                    int gradenum = int.Parse(Console.ReadLine());
+                    if (gradenum >= 90)
+
+                    {
+                        Console.WriteLine("Final grade A+");
+                    }
+                    else if (gradenum >= 80)
+                    {
+                        Console.WriteLine("Final grade A");
+                    }
+                    else if (gradenum >= 70)
+                    {
+                        Console.WriteLine("Final grade B+");
+                    }
+                    else if (gradenum >= 60)
+                    {
+                        Console.WriteLine("Final grade B");
+                    }
+                    else if (gradenum >= 50)
+                    {
+                        Console.WriteLine("Final grade C");
+                    }
+                    else if (gradenum >= 40)
+                    {
+                        Console.WriteLine("Final grade D");
+                    }
+                    else if (gradenum <= 39)
+                    { Console.WriteLine("Final grade F"); }
+
+
+
+
+
+
+                }
+
+
+
+        }
+    }
+}
